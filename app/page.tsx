25161c9f4846b105ec5fcc0429d4c310d3da64c2
huywb <pthhuy1306@gmail.com>
2025-07-21 20:23:26 +0700

    Setup folder

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
 import Image from "next/image";
-import Todo from "../app/components/Todo.jsx"
 export default function Home() {
   return (
    <div className="flex items-center h-screen w-full justify-center">
-      <Todo></Todo>
+
    </div>
   );
 }
