9bd95685b36aebc31f290b395841d3442aac45ba
huywb <pthhuy1306@gmail.com>
2025-07-23 01:34:56 +0700

    Config

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,13 @@ import { useEffect, useState } from "react";
 
 import AddCard from "./components/AddCard";
 import axios from "axios";
-import { set } from "mongoose";
 export default function Home() {
   const [page, setPage] = useState(1);
   const [open, setOpen] = useState(false);  
   const [data, setData] = useState([]);
   const [filter, setFilter] = useState('');
   const [ListTask, setListTask] = useState([]);
+  const [search, setSearch] = useState('');
   useEffect(() => {
     const fetchData  = async () => {
       const response = await axios.get('http://localhost:3000/api');
@@ -22,7 +22,20 @@ export default function Home() {
   }, []);
   
   let filtered = data;
-  console.log(data);
+
+  const handleSearch = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const value = e.target.value;
+  setSearch(value);
+  if (value === '') {
+    setListTask(data);
+  } else {
+    filtered = data.filter((item: any) => item.title.toLowerCase().includes(value.toLowerCase()));
+    setListTask(filtered);
+  }
+  console.log(filtered)
+  console.log(data)
+}
+  console.log(search)
 
   const handleFilterChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
   const value = e.target.value;
@@ -44,10 +57,14 @@ export default function Home() {
             <option value="Completed">Completed</option>
             <option value="Incompleted">Incompleted</option>
           </select>
+          <div className="flex items-center w-[80%] justify-center">
+          <input type="text" onChange={(e:any) => handleSearch(e)} className="outline-none p-2 rounded-l-xl bg-gray-200 text-black w-[50%]"  placeholder="Search..." name="search" id=""  />
+          <button   className="bg-gray-200 text-black p-2 rounded-r-xl cursor-pointer">Search</button>
+          </div>
           <h1 onClick={() => setOpen(true)} className=" cursor-pointer hover:translate-x-1 bg-gray-200 text-black p-2 rounded-xl duration-300">Add Task</h1>
           </div>
           <div className="flex w-full h-auto flex-wrap bg-gray-600 p-2 rounded-md">
-            <ListTodo  ListTask={filter ? ListTask : data} page={page} />
+            <ListTodo  ListTask={filter || search ? ListTask : data} page={page} />
           </div>
           <div className="w-full flex items-center gap-4 justify-center">
             {[...Array(Math.ceil(data.length / 10))].map((_, i) => (
